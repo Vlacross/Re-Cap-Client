f3194d17a089c6db3e0b612efca366f5b93a6e82
Tim Vlasuk <timvlasuk@gmail.com>
2019-04-03 15:14:30 -0700

    adding store and reducer folder for redux

---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+
+
+export default combineReducers();
+
+/*This way all reducers are in a single file then imported here, and exported with 'combine reducer' inside an object literal */
