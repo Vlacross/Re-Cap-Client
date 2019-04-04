fdd5131fd868a36c9b7f84b6060468a0e3151fff
Tim Vlasuk <timvlasuk@gmail.com>
2019-04-04 16:01:52 -0700

    setting more setup- configuring Routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,30 @@
 import React, { Component } from 'react';
 import { Route, withRouter } from 'react-router-dom';
-import logo from './logo.svg';
+// import logo from '../logo.svg';
+
+import Dashboard from './dashboard/dashboard';
+import Header from './navBar/navBar';
+import LandingPage from './landing/landingPage';
+
 import './App.css';
-import Dashboard from './dashboard';
-import Header from './header';
-import LandingPage from './landingPage';
 
 class App extends Component {
 
-  render() {
+  
 
+  render() {
+console.log("app")
     return (
 
       
         <div className="App">
           <Header />
           <Route exact path="/" component={LandingPage} />
-          <Route exact path="/dashboard" component={Dashboard} />
+          {/* <Route exact path="/dashboard" component={Dashboard} /> */}
+          {/* <Route exact path="/Login" component={Login} />
           <Route exact path="/enroll" component={EnrollForm} />
+          <Route exact path="/offeredTypes" component={OfferedTypes} /> */}
+          {/* <Route exact path="/ourStory" component={OurStory} /> */}
         </div>
      
     );
