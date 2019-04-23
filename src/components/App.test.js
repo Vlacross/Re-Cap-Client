397bf35a580d48cae42491dfb317bf1e363fab88
Tim Vlasuk <timvlasuk@gmail.com>
2019-04-23 10:43:32 -0700

    reverted previous troubleshooting and moved to introducing testing

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
+
+import { Provider } from 'react-redux';
+import Store from '../store';
+
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  shallow(<App />)
 });
