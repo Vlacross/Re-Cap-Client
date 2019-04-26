b24dab8f2bce46804fe5add12a7000eedd4eb3e0
Tim Vlasuk <timvlasuk@gmail.com>
2019-04-26 13:55:29 -0700

    set up testing for the  component

--- src/components/about/ourStory.test.js
+++ src/components/about/ourStory.test.js
@@ -1,10 +1,57 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import ReactDOM from 'react-dom';
-import OurStory from './ourStory';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+import { shallow, mount } from 'enzyme';
 
-it('renders without crashing', () => {
+import OurStoryConnected, { OurStory } from './ourStory';
+
+const bio = () => ({ bio: {bio: 'connected bibliography'} })
+
+let mockStore = configureMockStore();
+let store = mockStore(bio)
+
+describe('OurStory function', () => {
+
+	it('connected component renders without crashing', () => {
+		
+		shallow(<OurStoryConnected />)
+	});
+
+	it('component on its own renders without crashing', () => {
+		
+		shallow(<OurStory />)
+		
+	});
+
+	it('renders something', () => {
+		const wrapper = shallow(<OurStoryConnected />)
+		expect(wrapper).toBeDefined()
+	})
+
+	it('renders something', () => {
+		const wrapper = shallow(<OurStory />)
+		expect(wrapper).toBeDefined()
+	})
 	
-	const wrapper = shallow(<OurStory />)
-expect(wrapper).toBeDefined()
+	it('renders content based on passed props', () => {
+
+		const wrapper = shallow(<OurStory bio="bibliography"/>)
+		expect(wrapper.children.length).toBe(1)
+		expect(wrapper.find('.bioText').length).toBe(1)
+	});
+
+	it('renders content from connected state', () => {
+
+		const wrapper = mount(
+			<Provider store={store}>
+				<OurStoryConnected />
+			</Provider>
+		)
+		expect(wrapper.props('store').store.getState()).toEqual(bio())
+		expect(wrapper.find('.bioText').text().includes(bio().bio.bio)).toBeTruthy()
+
+	});
+	
+
 });
+
