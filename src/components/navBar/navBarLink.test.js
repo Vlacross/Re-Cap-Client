9e8c93929f1e0ebfc4828d2e0c455fd38260f40f
Tim Vlasuk <timvlasuk@gmail.com>
2019-04-26 21:48:52 -0700

    wrote tests for navBar link; still want to add jest testing for 'onclick' prop

--- src/components/navBar/navBarLink.test.js
+++ src/components/navBar/navBarLink.test.js
@@ -1,11 +1,42 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import ReactDOM from 'react-dom';
-import navBarLink from './navBarLink';
+import { shallow, mount } from 'enzyme';
+import { MemoryRouter } from 'react-router';
+import NavBarLink from './navBarLink';
+
+const mock = {
+			name: "mockName",
+			path: "/mockRoute"
+};
+
+it('renders something', () => { 
+
+	const wrapper = mount(	
+		<MemoryRouter>
+			<NavBarLink path={mock.path} name={mock.name}/>
+		</MemoryRouter>
+	)
+	expect(wrapper).toBeDefined()
+
+});
 
 it('renders without crashing', () => {
 	
-	const wrapper = shallow(<navBarLink />)
-	// console.log(wrapper)
-expect(wrapper).toBeDefined()
+	let entries = ["/mockRoute", "/"]
+	let entryIndex = 1
+
+	const wrapper = mount(	
+
+		<MemoryRouter
+		initialEntries={entries} 
+		initialIndex={entryIndex}>
+			<NavBarLink path={mock.path} name={mock.name}/>
+		</MemoryRouter>
+	)
+	expect(wrapper).toBeDefined()
+	expect(wrapper.children().props().children.props.path).toEqual(mock.path)
+	expect(wrapper.children().props().children.props.name).toEqual(mock.name)
+	expect(wrapper.children().props().history.location.pathname).toEqual(entries[entryIndex])
+
+
+
 });
