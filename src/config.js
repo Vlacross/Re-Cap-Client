1c2e678a3b94edebfbc7ce9b7198bd82e23ba329
Tim Vlasuk <timvlasuk@gmail.com>
2019-08-23 11:47:46 -0700

    updated local var to reflect accurate api destination with env variable based logic

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const API_URI = process.env.REACT_APP_API_URI || 'http://localhost:8080/';
 
+export const API_URI =  process.env.NODE_ENV === 'development' ? 'http://localhost:8080/' : process.env.REACT_APP_API_URI;
