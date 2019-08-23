87315166a196d269e58399f595533782ce57b5c0
Tim Vlasuk <timvlasuk@gmail.com>
2019-08-23 11:46:01 -0700

    reformatted text in the 'about' section, updated tests to reflect changes. Altered styling to fit screen better, implemented scrollbar styling

--- src/components/about/ourStory.js
+++ src/components/about/ourStory.js
@@ -6,9 +6,28 @@ import './ourStory.css';
   
   return (
     <div className="ourStory">
-      <h4 className="bioText">
-        {props.bio}
+      <h4 className="bioText who">
+        {props.bio && props.bio.who}
       </h4>
+
+      <h4 className="bioText what">
+        {props.bio && props.bio.what}
+      </h4>
+
+      <h4 className="bioText when">
+        {props.bio && props.bio.when}
+      </h4>
+
+      <h4 className="bioText where">
+        {props.bio && props.bio.where}
+      </h4>
+
+      <h4 className="bioText whoNow">
+        {props.bio && props.bio.whoNow}
+      </h4>
+      {/* <h4 className="bioText">
+        {props.bio}
+      </h4> */}
     </div>
   );
 };
--- src/components/about/ourStory.test.js
+++ src/components/about/ourStory.test.js
@@ -5,10 +5,18 @@ import { shallow, mount } from 'enzyme';
 
 import OurStoryConnected, { OurStory } from './ourStory';
 
-const bio = () => ({ bio: {bio: 'connected bibliography'} });
+const bio = () => ({ bio: {bio: mockBio} });
 
 let mockStore = configureMockStore();
 let store = mockStore(bio);
+
+let mockBio = {
+	who: 'who',
+	what: 'what',
+	when: 'when',
+	where: 'where',
+	whoNow: 'whoNow'
+}
 
 describe('OurStory function', () => {
 
@@ -35,9 +43,9 @@ describe('OurStory function', () => {
 	
 	it('renders content based on passed props', () => {
 
-		const wrapper = shallow(<OurStory bio="bibliography"/>)
+		const wrapper = shallow(<OurStory bio={mockBio} />)
 		expect(wrapper.children.length).toBe(1)
-		expect(wrapper.find('.bioText').length).toBe(1)
+		expect(wrapper.find('.bioText').length).toBe(Object.keys(mockBio).length)
 	});
 
 	it('renders content from connected state', () => {
@@ -47,8 +55,13 @@ describe('OurStory function', () => {
 				<OurStoryConnected />
 			</Provider>
 		)
+
 		expect(wrapper.props('store').store.getState()).toEqual(bio())
-		expect(wrapper.find('.bioText').text().includes(bio().bio.bio)).toBeTruthy()
+		expect(wrapper.find('.who').text().includes(bio().bio.bio.who)).toBeTruthy()
+		expect(wrapper.find('.what').text().includes(bio().bio.bio.what)).toBeTruthy()
+		expect(wrapper.find('.when').text().includes(bio().bio.bio.when)).toBeTruthy()
+		expect(wrapper.find('.where').text().includes(bio().bio.bio.where)).toBeTruthy()
+		expect(wrapper.find('.whoNow').text().includes(bio().bio.bio.whoNow)).toBeTruthy()
 
 	});
 	
