7dec0603eeaf62e8f58d7c87e4eb3daabc7b42ce
Tim Vlasuk <timvlasuk@gmail.com>
2019-11-13 12:47:45 -0800

    added autofill button for demo account and password reveal for input

--- src/components/loginForm/loginForm.js
+++ src/components/loginForm/loginForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Field, reduxForm, focus } from 'redux-form';
+import { Field, reduxForm, focus, change } from 'redux-form';
 import { connect } from 'react-redux';
-
+import { toggleShowPass } from '../../actions';
 import Input from '../input/input';
 import { authFetch } from '../../actions';
 import { required, notEmpty, trimmed, lengthMax, lengthMin, isLower } from '../../validators';
@@ -10,54 +10,86 @@ import dashboardRedirect from '../checkAuth/dashboardRedirect';
 
 import './loginForm.css';
 
+const demoAccount = {
+  username: 'jonjon',
+  password: 'unhashed'
+}
+
 let minLength = lengthMin(4);
 let maxLength = lengthMax(11);
 
 export class LoginForm extends React.Component {
-  
 
     onSubmit(values, dispatch) {
       dispatch(authFetch(values))
     };
 
+    autoFill(e) {
+      e.preventDefault();
+      console.log(this.props)
+      this.props.change('username', demoAccount.username)
+      this.props.change('password', demoAccount.password)
+    };
+
+    showPass(e) {
+      e.preventDefault();
+      this.props.dispatch(toggleShowPass())
+    }
+
     
   render() {
       
 
-      const {  handleSubmit, pristine, submitting } = this.props
+  const {  handleSubmit, pristine, submitting, view } = this.props
 
-    
-    
-      let error
-      if(this.props.error) {
-        error = <div className="formError">{this.props.error}</div>
-  }
+  let passView = view ? 'text' : 'password';
+
+  let error
+  if(this.props.error) {
+    error = <div className="formError">{this.props.error}</div>
+  } 
+  console.log('bigs')
+
       
   return(
     <form
       className="loginForm"
       onSubmit={handleSubmit(this.onSubmit)}>
     {error}
-    
-    <div><span className="demo">DemoUser: jonjon</span></div>
-
     <Field 
       name="username"
       type="text"
       component={Input}
       label="Username" 
-      validate={[required, notEmpty, trimmed, minLength, maxLength, isLower]} />
-
-<div><span className="demo">DemoPassword: unhashed</span></div>
+      login={true}
+      validate={[required, notEmpty, trimmed, minLength, maxLength, isLower]} 
+      />
     
     <Field 
       name="password"
-      type="text"
+      type={passView}
       component={Input}
       label="Password"
+      login={true}
       validate={[required, notEmpty, trimmed, minLength, maxLength, isLower]} />
 
     <div className="formInput">
+    <div className="input-controls">
+
+      <button
+        className="passShow loginButton"
+        onClick={(e) => this.showPass(e)}>
+          Show Pass
+      </button>
+
+      <button
+        className="autoFill loginButton"
+        onClick={(e) => this.autoFill(e)}>
+          Try the Demo
+      </button>
+
+    </div>
+
       <button
       className="loginButton"
        type="submit"
@@ -75,13 +107,15 @@ export class LoginForm extends React.Component {
 
 };
 
+
   
   LoginForm = connect(
     state => ({
       state: state,
       loggedIn: state.auth.user !== null,
       initialValues: state.auth.user,
-      error: state.auth.error
+      error: state.auth.error,
+      view: state.views.showPass
     }))(LoginForm)
 
      LoginForm = reduxForm({
